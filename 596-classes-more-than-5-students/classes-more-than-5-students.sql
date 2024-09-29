118e4c9fb5f8d98422310f5186fe014b4ef47864
Tirthesh Patange <92396028+tirthesh121@users.noreply.github.com>
2024-09-29 17:54:10 +0530

    Time: 379 ms (16.45%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-more-than-5-students/classes-more-than-5-students.sql
+++ 596-classes-more-than-5-students/classes-more-than-5-students.sql
@@ -1,5 +1,2 @@
 # Write your MySQL query statement below
-select distinct class
-from courses
-group by class
-having count(student) >= 5;
+select distinct class from courses group by class having count(student) >= 5;
